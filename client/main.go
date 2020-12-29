570786115fce98b160a33f28f37bf462f60c8ce2
flamingo <lion.zhang@mail.com>
2020-12-29 17:51:22 +0800

    add business on client project

--- client/main.go
+++ client/main.go
@@ -1,1 +1,11 @@
-package client
+package main
+
+import "github.com/kataras/iris/v12"
+
+func main() {
+	app := iris.Default()
+	app.Handle("GET", "/hello", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"message": "hello!!!"})
+	})
+	app.Listen(":8080")
+}
